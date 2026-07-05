bd0be52b4d03954b2fd66f20a35c15e9bd0d151c
ManoharlalsutharWeb <manoharlalsuthar1379@gmail.com>
2026-07-05 18:14:32 +0530

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { WagmiProvider } from 'wagmi'
+import { sepolia } from 'wagmi/chains'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const config = getDefaultConfig({
+  appName: 'Jangid Domains',
+  projectId: '1ab836da701970a88e87f0e998239db0',
+  chains: [sepolia],
+})
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+)
